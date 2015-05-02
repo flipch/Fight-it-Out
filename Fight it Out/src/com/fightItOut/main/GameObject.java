b47e8059aa85157555577e9903654f9882652f4e
Felipe Heliszkowski <felipech@outlook.com>
2015-05-02 14:04:19 +0100

    Added alot of new stuff. Included Java-doc. Created Player Object

---
+++ Fight it Out/src/com/fightItOut/main/GameObject.java
@@ -0,0 +1,113 @@
+package com.fightItOut.main;
+
+import java.awt.Graphics;
+
+/**
+ * Joins all Object types into this abstract one( GameObject) So enemyObject
+ * becomes the same as playerObject
+ * 
+ * @author Felipe Heliszkowski
+ * @since May 2, 2015
+ */
+public abstract class GameObject {
+
+	protected int x, y;
+	protected ID id;
+	protected int velX, velY;
+
+	
+	/**
+	 * @param x the position on screen X-wise
+	 * @param y the position on screen Y-wise
+	 * @param id the ID 
+	 */
+	public GameObject(int x, int y, ID id) {
+		this.x = x;
+		this.y = y;
+		this.id = id;
+	}
+
+	public abstract void tick();
+
+	/**
+	 * The rendering properties of the GameObject
+	 * @param g the graphics instance
+	 */
+	public abstract void render(Graphics g);
+
+	/**
+	 * @return the x
+	 */
+	public int getX() {
+		return x;
+	}
+
+	/**
+	 * @param x
+	 *            the x to set
+	 */
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	/**
+	 * @return the y
+	 */
+	public int getY() {
+		return y;
+	}
+
+	/**
+	 * @param y
+	 *            the y to set
+	 */
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public ID getId() {
+		return id;
+	}
+
+	/**
+	 * @param id
+	 *            the id to set
+	 */
+	public void setId(ID id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the velX
+	 */
+	public int getVelX() {
+		return velX;
+	}
+
+	/**
+	 * @param velX
+	 *            the velX to set
+	 */
+	public void setVelX(int velX) {
+		this.velX = velX;
+	}
+
+	/**
+	 * @return the velY
+	 */
+	public int getVelY() {
+		return velY;
+	}
+
+	/**
+	 * @param velY
+	 *            the velY to set
+	 */
+	public void setVelY(int velY) {
+		this.velY = velY;
+	}
+
+}
