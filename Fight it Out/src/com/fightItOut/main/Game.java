8d7853aa8b6b30f51713562adfffaac209a1dec4
Felipe Heliszkowski <felipech@outlook.com>
2015-05-02 13:22:46 +0100

    Initialized threads and mantains an average of 4.5k fps.

--- Fight it Out/src/com/fightItOut/main/Game.java
+++ Fight it Out/src/com/fightItOut/main/Game.java
@@ -1,31 +1,92 @@
 package com.fightItOut.main;
 
 import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
 
 /**
  * @author Felipe Heliszkowski
- * @version 1.0.1
+ * @version 1.0.2
  * @category Game
  */
-public class Game extends Canvas implements Runnable{
-	
+public class Game extends Canvas implements Runnable {
+
 	private static final long serialVersionUID = -1539400485938755709L;
 
 	public static final int WIDTH = 1280, HEIGHT = WIDTH / 12 * 9;
-	
-	public Game(){
+
+	private Thread thread;
+	private boolean running = false;
+
+	public Game() {
 		new Window(WIDTH, HEIGHT, "Fight it out!", this);
 	}
-	
-	public synchronized void start(){
-		
+
+	public synchronized void start() {
+		thread = new Thread(this);
+		thread.start();
+		running = true;
 	}
-	
-	public void run(){
-		
+
+	public synchronized void stop() {
+		try {
+			thread.join();
+			running = false;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
-	
-	public static void main(String args[]){
+
+	public void run() {
+		long lastTime = System.nanoTime();
+		double amountOfTicks = 60.0;
+		double ns = 1000000000 / amountOfTicks;
+		double delta = 0;
+		long timer = System.currentTimeMillis();
+		int frames = 0;
+		while (running) {
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			while (delta >= 1) {
+				tick();
+				delta--;
+			}
+			if (running)
+				render();
+			frames++;
+
+			if (System.currentTimeMillis() - timer > 1000) {
+				timer += 1000;
+				System.out.println("FPS: " + frames);
+				frames = 0;
+			}
+		}
+		stop();
+	}
+
+	private void tick() {
+
+	}
+
+	private void render() {
+		BufferStrategy bs = this.getBufferStrategy();
+		if (bs == null) {
+			this.createBufferStrategy(3);
+			return;
+		}
+
+		Graphics g = bs.getDrawGraphics();
+
+		g.setColor(Color.green);
+		g.fillRect(0, 0, WIDTH, HEIGHT);
+
+		g.dispose();
+		bs.show();
+	}
+
+	public static void main(String args[]) {
 		new Game();
 	}
 
