016363eaf3439f087cd8e8ca40f430fa1749309c
Felipe Heliszkowski <felipech@outlook.com>
2015-05-02 23:12:24 +0100

    a

---
+++ Fight it Out/src/com/fightItOut/main/Enemy_Ranged.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package com.fightItOut.main;
+
+import java.awt.Graphics;
+
+/**
+ * @author Felipe
+ * @version 1.0.0
+ * @since May 2, 2015
+ *
+ */
+public class Enemy_Ranged extends GameObject {
+
+	/* (non-Javadoc)
+	 * @see com.fightItOut.main.GameObject#tick()
+	 */
+	@Override
+	public void tick() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see com.fightItOut.main.GameObject#render(java.awt.Graphics)
+	 */
+	@Override
+	public void render(Graphics g) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
