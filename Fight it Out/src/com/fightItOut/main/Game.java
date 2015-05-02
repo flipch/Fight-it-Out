7f27214f9763048b184f706bfed8263811a92076
Felipe Heliszkowski <felipech@outlook.com>
2015-05-02 13:02:35 +0100

    Initial Gui done, blank JFrame. Initial properties of Game.

--- Fight it Out/src/com/fightItOut/main/Game.java
+++ Fight it Out/src/com/fightItOut/main/Game.java
@@ -1,14 +1,32 @@
-/**
- * 
- */
 package com.fightItOut.main;
+
+import java.awt.Canvas;
 
 /**
  * @author Felipe Heliszkowski
- * @version 1.0.0
+ * @version 1.0.1
  * @category Game
- *
  */
-public class Game {
+public class Game extends Canvas implements Runnable{
 	
+	private static final long serialVersionUID = -1539400485938755709L;
+
+	public static final int WIDTH = 1280, HEIGHT = WIDTH / 12 * 9;
+	
+	public Game(){
+		new Window(WIDTH, HEIGHT, "Fight it out!", this);
+	}
+	
+	public synchronized void start(){
+		
+	}
+	
+	public void run(){
+		
+	}
+	
+	public static void main(String args[]){
+		new Game();
+	}
+
 }
